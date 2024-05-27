abdb1688290a8c851650738e3e33fd1091183547
hijiki51 <hibiki0719euph@gmail.com>
2024-05-27 21:59:06 +0900

    fix: 🐛 fix replace

--- main.py
+++ main.py
@@ -8,15 +8,14 @@ from prometheus_client import start_http_server, Enum
 
 
 load_dotenv()
-macs = (os.getenv("WINDOW_SENSORS") or "").split(",")
+macs = [ m.replace(":","") for m in (os.getenv("WINDOW_SENSORS") or "").split(",")]
 PROMETHEUS_WINDOW_STAT = Enum("switchbot_window_state","Window open or close",labelnames=macs,states=["open","close"])
 
 def contact_handler(mac, data):
     parsed = unpack(">BBi??B",data)
     status = contact.SWContactStatus(parsed[0],parsed[2],parsed[3],parsed[4],parsed[5])
     _stat = "open" if status.door == 1 else "close"
-    _mac = mac.replace(":","")
-    PROMETHEUS_WINDOW_STAT.labels(_mac).state(_stat)
+    PROMETHEUS_WINDOW_STAT.labels(mac).state(_stat)
     return
 
 
